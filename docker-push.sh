315c527b866b8900f25bfeabc95c4754962c0c5e
STONE <ochuba6@gmail.com.com>
2022-09-13 07:32:21 +0100

    refactored into microservice

--- docker-push.sh
+++ docker-push.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
+# set -euo pipefail
 
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker-compose -f docker-compose-build.yaml push
+# echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+# docker-compose -f docker-compose-build.yaml push
--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=vasilkosturski
-export POSTGRES_PASSWORD=fake_pass
-export POSTGRES_HOST=udagram.c0dds5jp4tiy.eu-central-1.rds.amazonaws.com
-export POSTGRES_DB=postgres
-export AWS_BUCKET=udagram-259984666282
-export AWS_REGION=eu-central-1
+export POSTGRES_USERNAME=chimezdev
+export POSTGRES_PASSWORD=chimezdev
+export POSTGRES_HOST=chimezdev.cyatmnuenxde.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=chimezdev
+export AWS_BUCKET=chimezdev-alx-p3
+export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=hello
 export URL=http://localhost:8100
